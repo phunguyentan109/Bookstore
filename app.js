3faeb59226a33857e7bf0c99bc3f090819aa6cd6
phulicon1998 <phulicon815490@gmail.com>
2018-03-20 16:59:16 +0700

    Authentication user
    - Create page for user to enter the user's account to register.
    - Done register functionality

--- app.js
+++ app.js
@@ -1,31 +1,59 @@
 // =============================================================
 // REQUIREMENT
 // =============================================================
-var express 	= require("express"),
-	app 		= express(),
-	bodyParser 	= require("body-parser"),
-	mongoose	= require("mongoose");
+var express 				= require("express"),
+	app 					= express(),
+	bodyParser 				= require("body-parser"),
+	mongoose				= require("mongoose"),
+	passport				= require("passport"),
+	LocalStrategy 			= require("passport-local"),
+	passportLocalMongoose 	= require("passport-local-mongoose"),
+	User 					= require("./models/userModel");
 
 // =============================================================
 // CONFIGURATION
 // =============================================================
-// Database connection
+// DATABASE CONNECTION
 mongoose.connect("mongodb://localhost/bookstore");
 
-// General configuration
+// GENERAL CONFIGURATION
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
+
+// AUTHENTICATION
+app.use(require("express-session")({
+	secret: "me",
+	resave: false,
+	saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // =============================================================
 // ROUTES
 // =============================================================
 app.get("/", function(req, res){
-	res.render("home");
+	res.render("landing");
 });
 
-app.get("/login", function(req, res){
-	res.render("login");
+app.get("/register", function(req, res){
+	res.render("register");
+});
+
+app.post("/register", function(req, res){
+	var newUser = new User({username: req.body.username});
+	User.register(newUser, req.body.password, function(err, user){
+		if(err)
+			return res.redirect("/register");
+		passport.authenticate("local")(req, res, function(){
+			console.log("Register successfully!");
+			res.redirect("/");
+		});
+	})
 })
 
 app.listen(3000, function(req, res){
